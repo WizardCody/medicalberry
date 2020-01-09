090ed3f5391e54d61a025d773c73aa3508f99019
lajzz <twardyjaroslaw@gmail.com>
2020-01-09 19:53:17 +0100

    nowe rzeczy

--- homeguard/admin.py
+++ homeguard/admin.py
@@ -5,7 +5,7 @@ from django.db.models.functions import TruncMinute
 import json
 
 
-from .models import Device, Kontrakton, DeviceType
+from .models import Device, Kontrakton, DeviceType, Gas
 
 # Register your models here.
 
@@ -34,8 +34,30 @@ class KontraktonAdmin(admin.ModelAdmin):
         extra_context = extra_context or {"chart_data":as_json}
         
         return super().changelist_view(request, extra_context=extra_context)
+        
+class GasAdmin(admin.ModelAdmin):
+    readonly_fields = ('device_type',)
+    
+    list_display = ('device', 'device_type', 'value', 'event_time',)
+    
+    list_filter = ['event_time']
+    
+    search_fields = ['device_name', 'value']
+    
+    def changelist_view(self, request, extra_context=None):
+        chart_data = (
+            Gas.objects.annotate(date=TruncMinute("event_time"))
+            .values("date")
+            .annotate(y=Avg("value"))
+            .order_by("-date")
+        )
+        as_json = json.dumps(list(chart_data), cls=DjangoJSONEncoder)
+        extra_context = extra_context or {"chart_data":as_json}
+        
+        return super().changelist_view(request, extra_context=extra_context)
 
 admin.site.register(Kontrakton, KontraktonAdmin)
+admin.site.register(Gas, KontraktonAdmin)
 admin.site.register(Device, DeviceAdmin)
 
 admin.site.register(DeviceType)
