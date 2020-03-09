62533a9c23b404a66805ee7a7b5ffc0bd78df3fd
WizardCody <hoho3d@gmail.com>
2020-03-09 18:51:50 +0100

    Update

--- install.rpi.sh
+++ install.rpi.sh
@@ -4,13 +4,13 @@ echo
 echo "\e[92mMEDICAL\e[94mBERRY\e[0m"
 echo
 
-pip3 install django
-pip3 install django-jet
-pip3 install libsass
-pip3 install feedparser
-pip3 install mysqlclient
-pip3 install django-allow-cidr
-pip3 install gunicorn
+pip3 install django==2.2.7
+pip3 install django-jet==1.0.8
+pip3 install django-allow-cidr==0.3.1
+pip3 install libsass==0.19.4
+pip3 install feedparser==5.2.1
+pip3 install mysqlclient==1.4.6
+pip3 install gunicorn==20.0.4
 
 sudo apt-get install nginx
 # to do: setup nginx service, make links
